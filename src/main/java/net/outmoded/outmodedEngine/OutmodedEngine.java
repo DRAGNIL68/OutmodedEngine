0169dff235be51c1bc69c0fb9ac2f232c4c9846b
DRAGNIL68 <DRAGNIL68@users.noreply.github.com>
2026-06-28 09:34:39 +0200

    Initial commit

---
+++ src/main/java/net/outmoded/outmodedEngine/OutmodedEngine.java
@@ -0,0 +1,16 @@
+package net.outmoded.outmodedEngine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OutmodedEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
